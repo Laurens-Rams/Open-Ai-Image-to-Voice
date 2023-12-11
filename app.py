fdc00667e40415878957dbc247baeb46d1c3fccc
laurensart <artoflaurens@gmail.com>
2023-12-11 11:56:59 +0100

    Final

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import os
+import threading
+from main import process_image_async, client
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'  # Ensure this directory exists
+imgur_client_id = '0580ace497b71fe'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        pass
     return render_template('index.html')
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['image']
+    prompt = request.form.get('prompt', '')  # Retrieve the prompt from the form
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filename)
+
+    def callback(audio_path, analysis_text):
+        return jsonify({
+            'audio_path': audio_path,
+            'analysis_text': analysis_text
+        })
+
+    threading.Thread(target=process_image_async, args=(imgur_client_id, filename, prompt, callback)).start()
+    return jsonify({'message': 'Processing image...'}), 202
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-import cv2
-import time
 import requests
-import base64
-import threading
-import pygame
 from openai import OpenAI
 from pathlib import Path
-from mutagen.mp3 import MP3  # New import for checking MP3 length
+import os
+import base64
+import time
 
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
+elevenlabs_api_key = os.getenv('ELEVENLABS_API_KEY')
 
 client = OpenAI(api_key=api_key)
-
-pygame.mixer.init()
 
 def upload_image_to_imgur(filename, client_id):
     headers = {'Authorization': f'Client-ID {client_id}'}
@@ -28,7 +22,6 @@ def upload_image_to_imgur(filename, client_id):
         try:
             response = requests.post('https://api.imgur.com/3/upload', headers=headers, data=data)
             if response.status_code == 200:
-                print("Image uploaded successfully.")
                 return response.json()['data']['link']
             else:
                 print(f"Failed to upload image: {response.status_code}, {response.text}")
@@ -36,100 +29,56 @@ def upload_image_to_imgur(filename, client_id):
             print(f"Error during image upload: {e}")
         time.sleep(1)
 
-    print("Failed to upload image after several attempts.")
     return None
 
+def analyze_image_with_openai(client, image_url, prompt):
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=[{"role": "user", "content": [prompt, {"type": "image_url", "image_url": {"url": image_url, "detail": "high"}}]}],
+            max_tokens=150
+        )
+        return response.choices[0].message
+    except Exception as e:
+        print(f"Exception in analyze_image_with_openai: {e}")
+        return None
 
-def analyze_image_with_openai(client, image_url):
-    response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
-        messages=[{"role": "user", "content": ["This is a snapshot of a video. Create a short voiceover script in the style of David Attenborough. Only include the narration. The second sentence should already focus on the action that is performed."
-, {"type": "image_url", "image_url": {"url": image_url, "detail": "high"}}]}],
-        max_tokens=150
-    )
-    return response.choices[0].message
-
-# possible prompts:
-# Tell me what object I'm holding in my hands. Tell me what's the text on the object say and what I could use it for.
-# Analyze the following artwork and provide a critique focusing on its style, color usage, composition, and emotional impact. Consider the elements of art such as line, shape, form, space, texture, and value. Discuss how these elements contribute to the overall aesthetic and meaning of the piece. Also, speculate about the possible artistic influences and the era it might belong to if relevant
-
-def text_to_speech_elevenlabs(text, filename, elevenlabs_api_key):
+def text_to_speech_elevenlabs(text, filename):
     url = "https://api.elevenlabs.io/v1/text-to-speech/ABfOeM3ERCjGV6vqOoSg"
     payload = {"text": text}
     headers = {
         "xi-api-key": elevenlabs_api_key,
         "Content-Type": "application/json"
     }
-
-    response = requests.request("POST", url, json=payload, headers=headers)
-
-    if response.status_code == 200:
-        # Assuming the API returns the audio file directly
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-        return True
-    else:
-        print(f"Error in TTS request: {response.status_code}, {response.text}")
+    try:
+        response = requests.request("POST", url, json=payload, headers=headers)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            return True
+        else:
+            print(f"Error in TTS request: {response.status_code}, {response.text}")
+            return False
+    except Exception as e:
+        print(f"Exception in text_to_speech_elevenlabs: {e}")
         return False
 
 
-
-def process_image_async(client, imgur_client_id, img_filename, callback):
-    def thread_function():
-        try:
-            image_url = upload_image_to_imgur(img_filename, imgur_client_id)
-            if image_url:
-                analysis_result = analyze_image_with_openai(client, image_url)
-                text = analysis_result.content if hasattr(analysis_result, 'content') else str(analysis_result)
-
-                # Convert text to speech and save
-                speech_filename = Path(__file__).parent / f'speech_output_{int(time.time())}.mp3'
-                text_to_speech_elevenlabs(text, speech_filename, "af078375fb276aa85b61e20c185945c4")
-
-
-                callback(str(speech_filename))
+def process_image_async(imgur_client_id, img_filename, prompt, callback):
+    try:
+        image_url = upload_image_to_imgur(img_filename, imgur_client_id)
+        if image_url:
+            analysis_result = analyze_image_with_openai(client, image_url, prompt)
+            if analysis_result:
+                speech_filename = Path('static/audio') / f'speech_output_{int(time.time())}.mp3'
+                if text_to_speech_elevenlabs(analysis_result, speech_filename):
+                    callback(str(speech_filename), analysis_result)
+                else:
+                    callback(None, None)
             else:
-                callback(None)
-        except Exception as e:
-            print(f"Error in thread: {e}")
-            callback(None)
-
-    threading.Thread(target=thread_function).start()
-
-# Main script
-OpenAI(api_key=api_key)
-imgur_client_id = '0580ace497b71fe'
-
-
-cap = cv2.VideoCapture(0)
-time.sleep(2)  # Allow time for webcam to initialize
-frame_ready = True
-next_audio_file = None
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to capture frame from webcam.")
-        continue
-    cv2.imshow('Webcam Feed', frame)
-    def is_valid_image(frame):
-        return not (frame.max() == frame.min())
-
-    # In the main loop, before processing the image
-    if frame_ready and not next_audio_file:
-        if is_valid_image(frame):
-            img_filename = 'webcam_image.jpg'
-            cv2.imwrite(img_filename, frame)
-            print("Processing new frame...")
-            process_image_async(client, imgur_client_id, img_filename, on_audio_ready)
-            frame_ready = False
+                callback(None, None)
         else:
-            print("Invalid frame captured.")
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("Exiting...")
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+            callback(None, None)
+    except Exception as e:
+        print(f"Error in processing: {e}")
+        callback(None, None)
